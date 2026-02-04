2207518738927014f72179b1c999f880834d44a8
Roger Zheng <2015112456@ub.edu.bz>
2026-02-04 00:58:39 -0600

    Complete Part 3: Object Literals

---
+++ exercise3.js
@@ -0,0 +1,21 @@
+console.log("Part 3: Exercise 4");
+const student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    studentId: '12345',
+    courses: [],
+    getFullName() {
+        return this.firstName + " " + this.lastName;
+    },
+    enrollCourse(courseName) {
+        this.courses.push(courseName);
+    },
+    getCourseCount() {
+        return this.courses.length;
+    }
+};
+
+console.log(student.getFullName());
+student.enrollCourse('CMPS2212');
+student.enrollCourse('MATH2210');
+console.log("Course count: " + student.getCourseCount()); // Expected: 2
