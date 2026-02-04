bfd3f639302d0d73cebefe0302be882aeb813b3c
Roger Zheng <2015112456@ub.edu.bz>
2026-02-04 05:27:12 -0600

    Complete Part 4: Object.create() and Prototypes

--- exercise6.js
+++ exercise6.js
@@ -23,5 +23,5 @@ manager.addTeamMember('Jeff');
 console.log(`Team members: ${manager.team}`); //returns team from manager
 console.log(`Company: ${manager.company}`); //returns company manager -> employee prototype
 console.log(`Species: ${manager.species}`); //returns species from manager -> employee -> person prototype
-console.log(`${Object.hasOwn(manager, 'position')}`); //calls hasOwnProperty() from manager -> employee -> person -> Object.prototype
+console.log(`${Object.hasOwn(manager, 'position')}`); //calls hasOwn() from manager -> employee -> person -> Object.prototype
 console.log(`${manager.dob}`); //returns undefined as dob is not defined in the prototype chain
