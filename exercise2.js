f9f0e17bafb845e2caab091f726f8920c03b7a46
Roger Zheng <2015112456@ub.edu.bz>
2026-02-04 00:58:39 -0600

    Complete Part 3: Object Literals

--- exercise2.js
+++ exercise2.js
@@ -6,7 +6,7 @@ const squared = numbers.map(num => num * num);
 const evens = numbers.filter(num => num % 2 === 0);
 
 const sum = numbers.reduce((total, num) => {
-    total += num
+    return total + num;
 }, 0);
 
 console.log("Squared: " + squared);
