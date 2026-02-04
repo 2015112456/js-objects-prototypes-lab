2156e587a1688aa2616474e438f7817c0f5b586b
Roger Zheng <2015112456@ub.edu.bz>
2026-02-04 00:14:47 -0600

    Complete Part 1: Function declarations and expressions

--- exercise1.js
+++ exercise1.js
@@ -5,3 +5,11 @@ function calculateArea(length, width) {
 console.log(calculateArea(5, 10)); // Expected: 50
 console.log(calculateArea(7, 3)); // Expected: 21
 console.log(calculateArea(4, 4)); // Expected: 16
+
+console.log("Part 1: Exercise 2");
+const calculatePerimeter = function(length, width) {
+    return 2 * (length + width);
+};
+console.log(calculatePerimeter(5, 10)); // Expected: 30
+console.log(calculatePerimeter(7, 3)); // Expected: 20
+console.log(calculatePerimeter(4, 4)); // Expected: 16
